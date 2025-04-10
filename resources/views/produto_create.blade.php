a8423ea05ea4c8f4af0c720852f0c7a683684224
MaryRaquels <maria.lima2794@aluno.ce.gov.br>
2025-04-10 14:03:01 -0300

    ajuste no alert do form de create CRUD produtos

--- resources/views/produto_create.blade.php
+++ resources/views/produto_create.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto mt-8">
         <h1 class="text-xl font-bold mb-4">Novo Produto</h1>
 
-        <form x-data="{ showModal: false }" @submit.prevent="showModal = true" method="POST" action="{{ route('produtos.store') }}" class="bg-white p-6 rounded shadow-md">
+        <form x-data="{ showModal: false }" @submit.prevent="showModal = true" method="POST" action="{{ route('produtos.store') }}" class="bg-white p-6 rounded shadow-md ">
             @csrf
 
             <div class="mb-4">
@@ -61,11 +61,11 @@
 
             <div x-show="showModal" x-transition class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
                 <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-                    <h2 class="text-lg font-bold mb-4">Confirmar Cadastro</h2>
+                    <h2 class="text-lg font-bold mb-4 text-cyan-700">Confirmar Cadastro</h2>
                     <p class="mb-4">Tem certeza que deseja salvar este produto?</p>
                     <div class="flex justify-end gap-4">
                         <button type="button" @click="showModal = false" class="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded">Cancelar</button>
-                        <button type="button" @click="$el.closest('form').submit()" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">Confirmar</button>
+                        <button type="button" @click="$el.closest('form').submit()" class="bg-cyan-600 hover:bg-cyan-700 text-white px-4 py-2 rounded">Confirmar</button>
                     </div>
                 </div>
             </div>
